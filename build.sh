495cccc66a0bef0d55f86a17a216258310460cc1
Duc Hoang <minhducducminh1999@gmail.com>
2026-02-14 15:06:01 +0700

    install esbuild to build faster

---
+++ build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# rm everything
+rm assets/dist/*
+
+# esbuild
+npx esbuild \
+client/js/faker.js \
+client/js/main.js \
+--bundle \
+--minify \
+--outdir=assets/dist \
+--platform=browser
+
+# babel
+npx babel assets/dist/main.js --out-dir assets/dist \
+--presets=@babel/preset-env
+
+npx terser assets/dist/main.js \
+-o assets/dist/main.js -c -m
+echo "Minified main.js."
+
+# global
+npx terser \
+assets/lazysizes.min.js \
+assets/handlebars.min.js \
+assets/jquery-3.5.1.min.js \
+assets/vendor.js \
+assets/debounce.js \
+assets/enquire.min.js \
+assets/jquery.countdown.js \
+assets/slick.js \
+assets/lodash.min.js \
+assets/velocity.min.js \
+assets/image.zoom.js \
+assets/jquery.tmpl.min.js \
+-o assets/dist/global.min.js \
+-c -m
+echo "Minified global.min.js."
+
+# wpbingo
+npx terser \
+assets/wpbingo.js \
+assets/facets.js \
+assets/predictive-search.js \
+-o assets/dist/wpbingo.min.js \
+-c -m
+echo "Minified wpbingo.min.js."
+
+# pdp
+npx terser \
+assets/recent-product.min.js \
+assets/360imagerotate.js \
+assets/photoswipe.min.js \
+assets/photoswipe-ui-default.min.js \
+-o assets/dist/product.min.js \
+-c -m
+echo "Minified product.min.js."
